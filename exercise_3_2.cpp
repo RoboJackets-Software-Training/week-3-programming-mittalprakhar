177dfa682ee3c1cd518224f213a253e91f4a11b1
mittalprakhar <mittalprakhar57@gmail.com>
2020-10-02 16:14:36 +0000

    Finished Exercise 3.2

--- exercise_3_2.cpp
+++ exercise_3_2.cpp
@@ -6,25 +6,82 @@ LinkedList::LinkedList() {
 }
 
 void LinkedList::add_to_front(std::string name) {
-    return;
+    auto new_node = std::make_unique<Node>();
+    new_node->name = name;
+    new_node->next = std::move(head);
+    head = std::move(new_node);
 }
 
 void LinkedList::add_to_back(std::string name){
-    return;
+    auto new_node = std::make_unique<Node>();
+    new_node->name = name;
+    new_node->next = nullptr;
+    if (head == nullptr) {
+    	head = std::move(new_node);
+	} else {
+		Node* curr = head.get();
+		while (curr->next != nullptr) {
+			curr = curr->next.get();
+		}
+		curr->next = std::move(new_node);
+	}
 }
 
 void LinkedList::add_at_index(std::string name, int index) {
-    return;
+    auto new_node = std::make_unique<Node>();
+    new_node->name = name;
+    if (index != 0) {
+    	Node* curr = head.get();
+    	for (int i = 0; i < index - 1; i++) {
+    		curr = curr->next.get();
+    	}
+    	new_node->next = std::move(curr->next);
+    	curr->next = std::move(new_node);
+    } else {
+    	new_node->next = std::move(head);
+    	head = std::move(new_node);
+    }
 }
 
 void LinkedList::remove_from_front() {
-    return;
+    if (head != nullptr) {
+    	head = std::move(head->next);
+    }
 }
 
 void LinkedList::remove_from_back() {
-    return;
+    if (head != nullptr) {
+    	if (head->next != nullptr) {
+    		Node* curr = head.get();
+    		while (curr->next->next != nullptr) {
+    			curr = curr->next.get();
+    		}
+    		curr->next = nullptr;
+    	} else {
+    		head = nullptr;
+    	}
+    }
+}
+
+void LinkedList::remove_at_index(int index) {
+	if (head != nullptr) {
+		if (index != 0) {
+			Node* curr = head.get();
+			for (int i = 0; i < index - 1; i++) {
+				curr = curr->next.get();
+			}
+			curr->next = std::move(curr->next->next);
+		} else {
+			head = std::move(head->next);
+		}
+	}
 }
 
 void LinkedList::print_names() {
-    return;
+    Node* curr = head.get();
+    while (curr != nullptr) {
+    	std::cout << curr->name << "\t";
+    	curr = curr->next.get();
+    }
+    std::cout << "\n";
 }
